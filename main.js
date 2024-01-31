d80a57f9795be2ffbc7655760cd16e6a30f87aa1
Jackson Leech <37315894+DemoEncoder45@users.noreply.github.com>
2024-01-30 22:50:41 -0500

    Fixied the Add function. Text at the bottom of container.

--- main.js
+++ main.js
@@ -2,10 +2,20 @@ const add = document.getElementById("addItem")
 
 const input = document.getElementById("inputField")
 
+const taskContainer = document.getElementById('taskListContainer')
+
+const edit = document.getElementById("editButton")
+
+const erase = document.getElementById("deleteButton")
+
 add.addEventListener('click', function(){
     const item = document.createElement('p')
     const task = document.createElement('div')
-    item.innerText = inputField.value
+    task.classList.add('task-styling')
+    item.innerText = input.value
     task.appendChild(item)
-    inputField.value = ""
+    input.value = ""
+    taskContainer.appendChild(task)
+    
 })
+
