abd3c8c6c276ff3b72c553d81457b84089b23ba4
Jackson Leech <37315894+DemoEncoder45@users.noreply.github.com>
2024-02-01 23:13:55 -0500

    Error with implementing task options.

--- main.js
+++ main.js
@@ -7,12 +7,35 @@ const taskContainer = document.getElementById('taskListContainer')
 add.addEventListener('click', function(){
     const task = document.createElement('div')
     const item = document.createElement('p')
+    
+    const check = document.createElement('input')
+    check.setAttribute('type', 'checkbox')
+    check.setAttribute('data-is-task-complete', 'false')
+
+    const buttons = document.createElement('div')
+    const edit = document.createElement('button')
+    
+    const erase = document.createElement('button')
+   
+   
     task.classList.add('task-list__task')
     item.classList.add('task-list__task__title');
+    check.classList.add('task-list__task__options__check-task')
+    buttons.classList.add('task-list__task__options__buttons')
+    edit.classList.add('button button--edit')
+    erase.classList.add('button button--delete')
+
     item.innerText = input.value
     task.appendChild(item)
     input.value = ""
     taskContainer.appendChild(task)
+    task.appendChild(check)
+    buttons.appendChild(edit)
+    buttons.appendChild(erase)
+    
+    
+    
+
     
 })
 
